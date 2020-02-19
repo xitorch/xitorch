2e89a8c35a3cc8970a9af5e868a963766fac345d
mfkasim91 <firman.kasim@gmail.com>
2020-02-19 17:34:22 +0000

    With step argument and use torch.allclose in checking instead of ratio

--- lintorch/tests/utils.py
+++ lintorch/tests/utils.py
@@ -22,8 +22,8 @@ def device_dtype_float_test(only64=False, onlycpu=False):
         return fcn_all
     return device_dtype_float_test_fcn
 
-def compare_grad_with_fd(fcn, args, idxs, eps=1e-6, max_rtol=1e-3,
-        max_median_rtol=1e-4, fd_to64=True, verbose=False):
+def compare_grad_with_fd(fcn, args, idxs, eps=1e-6, rtol=1e-5,
+        atol=1e-8, fd_to64=True, verbose=False, step=1):
 
     args = [a.clone().detach().requires_grad_() if type(a) == torch.Tensor else a for a in args]
     device = args[idxs[0]].device
@@ -49,24 +49,10 @@ def compare_grad_with_fd(fcn, args, idxs, eps=1e-6, max_rtol=1e-3,
             for arg in args]
     else:
         argsfd = args
-    fds = [finite_differences(fcn, argsfd, idx, eps=eps[i]) for i,idx in enumerate(idxs)]
+    fds = [finite_differences(fcn, argsfd, idx, eps=eps[i], step=step) for i,idx in enumerate(idxs)]
 
     for i in range(len(idxs)):
-        ratio = grads[i] / fds[i]
-        if verbose:
-            print("Params #%d" % (idxs[i]))
-            print("* grad:")
-            print(grads[i])
-            print("* fd:")
-            print(fds[i])
-            print("* ratio:")
-            print(ratio)
-
-        dev = (ratio-1.0).abs()
-        if max_rtol is not None:
-            assert dev.max() < max_rtol, "Max dev ratio: %.3e (tolerated: %.3e)" % (dev.max(), max_rtol)
-        if max_median_rtol is not None:
-            assert dev.median() < max_median_rtol, "Median dev ratio: %.3e (tolerated: %.3e)" % (dev.median(), max_median_rtol)
+        torch.allclose(grads[i], fds[i], rtol=rtol, atol=atol)
 
 def get_diagonally_dominant_class(na):
     class Acls(lt.Module):
