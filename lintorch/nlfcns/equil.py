82b11a762b5a40e6bc5ab185c54f7361dfc716b6
mfkasim91 <firman.kasim@gmail.com>
2020-04-08 13:37:12 +0100

    Rootfinder operation

--- lintorch/nlfcns/equil.py
+++ lintorch/nlfcns/equil.py
@@ -5,7 +5,18 @@ from lintorch.maths.rootfinder import lbfgs, selfconsistent, broyden, diis, grad
 from lintorch.fcns.solve import solve
 from lintorch.core.base import Module as LintorchModule
 
-__all__ = ["equilibrium"]
+__all__ = ["equilibrium", "rootfinder"]
+
+def rootfinder(fcn, y0, params, fwd_options={}, bck_options={}):
+    """
+    Solving the rootfinder equation of a given function,
+
+        0 = fcn(y, *params)
+
+    where `fcn` is a function that can be non-linear and produce output of shape
+    `y`. The output of this block is `y` that produces the 0 as the output
+    """
+    return _RootFinder.apply(fcn, y0, fwd_options, bck_options, *params)
 
 def equilibrium(fcn, y0, params, fwd_options={}, bck_options={}):
     """
@@ -17,6 +28,95 @@ def equilibrium(fcn, y0, params, fwd_options={}, bck_options={}):
     of `y`.
     """
     return _Equilibrium.apply(fcn, y0, fwd_options, bck_options, *params)
+
+class _RootFinder(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, fcn, y0, options, bck_options, *params):
+        # set default options
+        config = set_default_option({
+            "max_niter": 50,
+            "min_eps": 1e-9,
+            "verbose": False,
+            "linesearch": True,
+            "jinv0": 0.5,
+            "method": "lbfgs",
+        }, options)
+        ctx.bck_options = set_default_option({
+            "max_niter": 50,
+            "min_eps": 1e-9,
+            "verbose": False,
+        }, bck_options)
+
+        def loss(y):
+            yfcn = fcn(y, *params)
+            return yfcn
+
+        jinv0 = config["jinv0"]
+        method = config["method"].lower()
+        if method == "lbfgs":
+            y = lbfgs(loss, y0, **config)
+        elif method == "selfconsistent":
+            y = selfconsistent(loss, y0, **config)
+        elif method == "broyden":
+            y = broyden(loss, y0, **config)
+        elif method == "diis":
+            y = diis(loss, y0, **config)
+        elif method == "gradrca":
+            y = gradrca(loss, y0, **config)
+        else:
+            raise RuntimeError("Unknown method: %s" % config["method"])
+
+        ctx.fcn = fcn
+        ctx.params = params
+        ctx.yout = y
+        return y
+
+    @staticmethod
+    def backward(ctx, grad_yout):
+        yout = ctx.yout
+        params = ctx.params
+        nr = yout.shape[-1]
+        # dL/df
+        _apply_DfDy = _DfDy(nr, ctx.fcn)
+        gyfcn = solve(_apply_DfDy, [yout, *params], grad_yout.unsqueeze(-1),
+            fwd_options=ctx.bck_options, bck_options=ctx.bck_options).squeeze(-1)
+
+        # get the grad for the params
+        with torch.enable_grad():
+            params_copy = [p.clone() for p in params]
+            yfcn = ctx.fcn(yout, *params_copy)
+        grad_params = torch.autograd.grad(yfcn, params_copy, grad_outputs=gyfcn,
+            create_graph=torch.is_grad_enabled())
+
+        return (None, None, None, None, *grad_params)
+
+class _DfDy(LintorchModule):
+    def __init__(self, nr, fcn):
+        super(_ImDfDy, self).__init__(shape=(nr,nr), is_symmetric=False)
+        self.fcn = fcn
+        self.yinp = None
+        self.yfcn = None
+
+    def forward(self, gy, yfcn, *params):
+        gy = gy.squeeze(-1)
+        if torch.is_grad_enabled():
+            yfcn.requires_grad_()
+            with torch.enable_grad():
+                yout = self.fcn(yfcn, *params)
+            dfdy, = torch.autograd.grad(yout, (yfcn,), grad_outputs=gy,
+                create_graph=torch.is_grad_enabled())
+        else:
+            # only evaluate once to save running time
+            if self.yfcn is None:
+                with torch.enable_grad():
+                    self.yinp = yfcn.clone().detach().requires_grad_()
+                    self.yfcn = self.fcn(self.yinp, *params)
+            dfdy, = torch.autograd.grad(self.yfcn, (self.yinp,), gy,
+                retain_graph=True, create_graph=torch.is_grad_enabled())
+
+        res = -dfdy
+        res = res.unsqueeze(-1)
+        return res
 
 class _Equilibrium(torch.autograd.Function):
     @staticmethod
--- lintorch/tests/test_nlfcns.py
+++ lintorch/tests/test_nlfcns.py
@@ -4,6 +4,43 @@ from torch.autograd import gradcheck, gradgradcheck
 import lintorch as lt
 from lintorch.tests.utils import device_dtype_float_test
 
+torch.manual_seed(100)
+random.seed(100)
+dtype = torch.float64
+
+class PolynomialModule(torch.nn.Module):
+    def __init__(self):
+        super(PolynomialModule, self).__init__()
+
+    def forward(self, y, c):
+        # y: (nbatch, 1)
+        # c: (nbatch, nr)
+        nr = c.shape[1]
+        power = torch.arange(nr)
+        b = (y ** power * c).sum(dim=-1, keepdim=True) # (nbatch, 1)
+        return b
+
+@device_dtype_float_test()
+def test_rootfinder(dtype, device):
+    nr = 4
+    nbatch = 1
+    x  = torch.tensor([-1, 0, 4, 1]).unsqueeze(0).to(dtype).requires_grad_()
+    y0 = torch.rand((nbatch, 1)).to(dtype)
+    params = (x,)
+
+    model = PolynomialModule()
+    y = lt.rootfinder(model, y0, params)
+    f = model(y, *params)
+    assert torch.allclose(f*0, f)
+
+    def getloss(x, y0):
+        model = PolynomialModule()
+        y = lt.equilibrium(model, y0, (x,))
+        return y
+
+    gradcheck(getloss, (x, y0))
+    gradgradcheck(getloss, (x, y0))
+
 @device_dtype_float_test()
 def test_equil(dtype, device):
     class DummyModule(torch.nn.Module):
