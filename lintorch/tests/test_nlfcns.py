9837b89b4ac056fd3ef953faa5fb0ad77979930b
mfkasim91 <firman.kasim@gmail.com>
2020-04-08 13:42:17 +0100

    Refactor the test nonlinear functions

--- lintorch/tests/test_nlfcns.py
+++ lintorch/tests/test_nlfcns.py
@@ -4,10 +4,6 @@ from torch.autograd import gradcheck, gradgradcheck
 import lintorch as lt
 from lintorch.tests.utils import device_dtype_float_test
 
-torch.manual_seed(100)
-random.seed(100)
-dtype = torch.float64
-
 class PolynomialModule(torch.nn.Module):
     def __init__(self):
         super(PolynomialModule, self).__init__()
@@ -22,9 +18,13 @@ class PolynomialModule(torch.nn.Module):
 
 @device_dtype_float_test()
 def test_rootfinder(dtype, device):
+    torch.manual_seed(100)
+    random.seed(100)
+    dtype = torch.float64
+
     nr = 4
     nbatch = 1
-    x  = torch.tensor([-1, 0, 4, 1]).unsqueeze(0).to(dtype).requires_grad_()
+    x  = torch.tensor([-1, 1, 4, 1]).unsqueeze(0).to(dtype).requires_grad_()
     y0 = torch.rand((nbatch, 1)).to(dtype)
     params = (x,)
 
@@ -43,34 +43,22 @@ def test_rootfinder(dtype, device):
 
 @device_dtype_float_test()
 def test_equil(dtype, device):
-    class DummyModule(torch.nn.Module):
-        def __init__(self):
-            super(DummyModule, self).__init__()
-
-        def forward(self, y, c):
-            # y: (nbatch, 1)
-            # c: (nbatch, nr)
-            nr = c.shape[1]
-            power = torch.arange(nr)
-            b = (y ** power * c).sum(dim=-1, keepdim=True) # (nbatch, 1)
-            return b
-
     torch.manual_seed(100)
     random.seed(100)
-
     dtype = torch.float64
+
     nr = 4
     nbatch = 1
     x  = torch.tensor([-1, 1, 4, 1]).unsqueeze(0).to(dtype).requires_grad_()
     y0 = torch.rand((nbatch, 1)).to(dtype)
     params = (x,)
 
-    model = DummyModule()
+    model = PolynomialModule()
     y = lt.equilibrium(model, y0, params)
     assert torch.allclose(y, model(y, *params))
 
     def getloss(x, y0):
-        model = DummyModule()
+        model = PolynomialModule()
         y = lt.equilibrium(model, y0, (x,))
         return y
 
