49b070b78028f5cc9d336a82de698ec9f3f0bbff
mfkasim91 <firman.kasim@gmail.com>
2020-04-09 18:22:36 +0100

    Extract differentiable parameters function

--- lintorch/utils/misc.py
+++ lintorch/utils/misc.py
@@ -1,5 +1,18 @@
+import torch
+
 def set_default_option(defopt, opt=None):
     if opt is None:
         opt = {}
     defopt.update(opt)
     return defopt
+
+def extract_differentiable_tensors(model):
+    res = list(model.parameters())
+
+    # add differentiable and non-parameters attribute in the model
+    for varname in model.__dict__:
+        var = model.__dict__[varname]
+        if not isinstance(var, torch.Tensor): continue
+        if var.requires_grad:
+            res.append(var)
+    return res
