876cd9d40b814cd4cabf5a1b1b07a4d6c7584235
mfkasim91 <firman.kasim@gmail.com>
2020-04-27 11:31:02 +0100

    Use requires_grad_ for all params in solve

--- lintorch/fcns/solve.py
+++ lintorch/fcns/solve.py
@@ -109,7 +109,7 @@ class solve_torchfcn(torch.autograd.Function):
 
         # calculate the grad of matrices parameters
         with torch.enable_grad():
-            params = [p.clone() for p in ctx.params]
+            params = [p.clone().detach().requires_grad_() for p in ctx.params]
             loss = -ctx.A(ctx.x, *params) # (nbatch, nr, ncols)
 
         grad_params = torch.autograd.grad((loss,), params, grad_outputs=(v,),
