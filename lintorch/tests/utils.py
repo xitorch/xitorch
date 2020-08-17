f8f9e8846d2c700adf2a87b79cb6622b0f61b86a
mfkasim91 <firman.kasim@gmail.com>
2020-08-17 12:18:24 +0100

    Remove compare_grad_with_fd (deprecated)

--- lintorch/tests/utils.py
+++ lintorch/tests/utils.py
@@ -4,7 +4,7 @@ import pytest
 import argparse
 from lintorch.utils.fd import finite_differences
 
-__all__ = ["device_dtype_float_test", "compare_grad_with_fd", "get_diagonally_dominant_class"]
+__all__ = ["device_dtype_float_test", "get_diagonally_dominant_class"]
 
 def device_dtype_float_test(only64=False, onlycpu=False):
     dtypes = [torch.float, torch.float64]
@@ -21,38 +21,6 @@ def device_dtype_float_test(only64=False, onlycpu=False):
                     fcn(dtype, device)
         return fcn_all
     return device_dtype_float_test_fcn
-
-def compare_grad_with_fd(fcn, args, idxs, eps=1e-6, rtol=1e-5,
-        atol=1e-8, fd_to64=True, verbose=False, step=1):
-
-    args = [a.clone().detach().requires_grad_() if type(a) == torch.Tensor else a for a in args]
-    device = args[idxs[0]].device
-    if not hasattr(eps, "__iter__"):
-        eps = [eps for i in range(len(idxs))]
-
-    # calculate the differentiable loss
-    loss0 = fcn(*args)
-
-    # zeroing the grad
-    for idx in idxs:
-        if args[idx].grad is not None:
-            args[idx].grad.zero_()
-
-    loss0.backward()
-    grads = [args[idx].grad.data for idx in idxs]
-
-    # compare with finite differences
-    if fd_to64:
-        argsfd = [arg.to(torch.float64) \
-            if (type(arg) == torch.Tensor and arg.dtype == torch.float) \
-            else arg \
-            for arg in args]
-    else:
-        argsfd = args
-    fds = [finite_differences(fcn, argsfd, idx, eps=eps[i], step=step) for i,idx in enumerate(idxs)]
-
-    for i in range(len(idxs)):
-        torch.allclose(grads[i], fds[i], rtol=rtol, atol=atol)
 
 def get_diagonally_dominant_class(na):
     class Acls(lt.Module):
