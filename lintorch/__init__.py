c956725c7b1ba8237e2e22d3e71f10b7b2c3f3fe
mfkasim91 <firman.kasim@gmail.com>
2020-08-17 14:50:02 +0100

    Rename file rootfinder2 to rootfinder

--- lintorch/__init__.py
+++ lintorch/__init__.py
@@ -3,5 +3,5 @@ from lintorch.core.linop import *
 from lintorch.funcs.symeig import *
 from lintorch.funcs.jac import *
 from lintorch.funcs.solve import *
-from lintorch.funcs.rootfinder2 import *
+from lintorch.funcs.rootfinder import *
 from lintorch.utils.debugmodes import *
