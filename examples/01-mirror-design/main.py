9b2dcef78d0d235751feac0d26f736513b879c94
mfkasim91 <firman.kasim@gmail.com>
2020-09-07 16:14:53 +0100

    Minor adjustment in the mirror example

--- examples/01-mirror-design/main.py
+++ examples/01-mirror-design/main.py
@@ -5,10 +5,7 @@ import lintorch as lt
 from lintorch.optimize import rootfinder
 import matplotlib.pyplot as plt
 
-class Gaussian(torch.nn.Module):
-    def forward(self, x):
-        return torch.exp(-x*x)
-
+################### neural network for the surface ###################
 class SimpleNN(torch.nn.Module):
     def __init__(self, ndim):
         super(SimpleNN, self).__init__()
@@ -32,6 +29,7 @@ class SimpleNN(torch.nn.Module):
         z = znn * (1-torch.tanh((radsurf-3.0) * 5.0))
         return torch.cat((rsurf, z), dim=-1) # (nbatch, ndim)
 
+################### physics functions ###################
 def get_intersection(r0, v, fcn):
     # r0: (nbatch, ndim) initial point of the rays
     # v: (nbatch, ndim) the direction of travel of the rays
@@ -44,7 +42,7 @@ def get_intersection(r0, v, fcn):
 
     y0 = torch.zeros_like(v)
     fwd_options = {
-        "method": "np_broyden1",
+        "method": "np_broyden1", # more robust
     }
     y = rootfinder(rootfinder_fcn, y0, params=(r0, v), fwd_options=fwd_options)
     return y[...,:-1], y[...,-1:] # (nbatch, ndim-1) and (nbatch, 1)
@@ -53,8 +51,7 @@ def get_normal(rsurf, fcn):
     nbatch, ndimm1 = rsurf.shape
     allv = torch.eye(ndimm1, dtype=rsurf.dtype, device=rsurf.device).unsqueeze(0).repeat(nbatch, 1, 1) # (nbatch, ndim-1, ndim-1)
 
-    grad_enabled = torch.is_grad_enabled()
-    dfdys = [jvp(fcn, rsurf, v=allv[...,i], create_graph=grad_enabled)[1] for i in range(ndimm1)] # ndim-1, each (nbatch, ndim)
+    dfdys = [jvp(fcn, rsurf, v=allv[...,i], create_graph=torch.is_grad_enabled())[1] for i in range(ndimm1)] # ndim-1, each (nbatch, ndim)
     normal = torch.cross(dfdys[0], dfdys[1], dim=-1) # (nbatch, ndim)
     normal = normal / normal.norm(dim=-1, keepdim=True)
     return normal
@@ -68,6 +65,7 @@ def get_reflection(r0, v, fcn):
     v1 = v1 / v1.norm(dim=-1, keepdim=True)
     return r1, v1
 
+################### plotting functions ###################
 def plot_rays(r0, v, t0, t1, xyidx=(0,1)):
     rini = r0 + v * t0 # (nbatch, ndim)
     rfin = r0 + v * t1 # (nbatch, ndim)
@@ -83,6 +81,7 @@ def plot_surface(fcn, dtype):
     zsurf = fcn(rsurf)[:,-1] # (nbatch, 1)
     plt.plot(xsurf.view(-1).detach().numpy(), zsurf.view(-1).detach().numpy())
 
+################### setup functions ###################
 def generate_rays(nrays):
     phisource = torch.rand((nrays, 1), dtype=dtype) * (2*np.pi)
     thetasource = torch.rand((nrays, 1), dtype=dtype) * (np.pi/6.)
@@ -125,9 +124,10 @@ if __name__ == "__main__":
         # reflected by the neural-network mirror
         r1, v1 = get_reflection(r0source, vsource, nn.forward)
         # captured by the screen
+        rscreen, t1 = get_intersection(r1, v1, screen_fcn)
+        # compute how far the rays are from the target
         xtarget = 1.0
         ytarget = 0.0
-        rscreen, t1 = get_intersection(r1, v1, screen_fcn)
         devx = rscreen[:,0].reshape(-1) - xtarget
         devy = rscreen[:,1].reshape(-1) - ytarget
         loss = torch.dot(devx, devx) + torch.dot(devy, devy)
