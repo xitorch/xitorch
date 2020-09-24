1a4cd7a361326ddcbd94e10a34e1d11b6d365044
mfkasim91 <firman.kasim@gmail.com>
2020-09-24 10:35:42 +0100

    Jac to test scalar function

--- xitorch/_tests/test_jac.py
+++ xitorch/_tests/test_jac.py
@@ -16,6 +16,11 @@ def hfunc1(A, b, x0):
     x = torch.nn.Softplus()(x)
     return x.sum()
 
+def scalar_func(A, b, x0):
+    x = A * x0 + b
+    x = torch.nn.Softplus()(x)
+    return x
+
 class func2(EditableModule):
     def __init__(self, b):
         self.b = b
@@ -45,6 +50,12 @@ class hfunc2(EditableModule):
         x = torch.matmul(A * self.b, x0) + b
         x = torch.nn.Softplus()(x)
         return x.sum()
+
+def getfnscalarparams():
+    A = torch.rand((), dtype=dtype, requires_grad=True)
+    b = torch.rand((), dtype=dtype, requires_grad=True)
+    x = torch.rand((), dtype=dtype, requires_grad=True)
+    return (A, b, x)
 
 def getfnparams(na):
     A = torch.rand((na,na), dtype=dtype, requires_grad=True)
@@ -165,6 +176,42 @@ def test_jac_method_grad():
         gradgradcheck(fcnr, (i, v, *nnparams, *params))
         gradcheck    (fcnl, (i, w[i], *nnparams, *params))
         gradgradcheck(fcnl, (i, w[i], *nnparams, *params))
+
+def test_jac_scalar_func():
+    params = getfnscalarparams()
+    nparams = len(params)
+    all_idxs = [None, (0,), (1,), (0,1), (0,1,2)]
+    func = scalar_func
+
+    for idxs in all_idxs:
+        if idxs is None:
+            gradparams = params
+        else:
+            gradparams = [params[i] for i in idxs]
+
+        jacs = jac(func, params, idxs=idxs)
+        assert len(jacs) == len(gradparams)
+
+        y = func(*params)
+        nout = torch.numel(y)
+        nins = [torch.numel(p) for p in gradparams]
+        v = torch.rand_like(y).requires_grad_()
+        for i in range(len(jacs)):
+            assert list(jacs[i].shape) == [nout, nins[i]]
+
+        # get rmv
+        jacs_rmv = torch.autograd.grad(y, gradparams, grad_outputs=v, create_graph=True)
+        # the jac LinearOperator has shape of (nout, nin), so we need to flatten v
+        jacs_rmv0 = [jc.rmv(v.view(-1)) for jc in jacs]
+
+        # calculate the mv
+        w = [torch.rand_like(p) for p in gradparams]
+        jacs_lmv = [torch.autograd.grad(jacs_rmv[i], (v,), grad_outputs=w[i], retain_graph=True)[0] for i in range(len(jacs))]
+        jacs_lmv0 = [jacs[i].mv(w[i].view(-1)) for i in range(len(jacs))]
+
+        for i in range(len(jacs)):
+            assert torch.allclose(jacs_rmv[i].view(-1), jacs_rmv0[i].view(-1))
+            assert torch.allclose(jacs_lmv[i].view(-1), jacs_lmv0[i].view(-1))
 
 def test_hess_func():
     na = 3
