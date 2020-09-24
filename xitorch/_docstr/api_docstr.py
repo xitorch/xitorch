4e9efe20dbbbeb49915e81bda6a4104c7b124f59
mfkasim91 <firman.kasim@gmail.com>
2020-09-24 15:39:34 +0100

    show default values of method-specific kwargs

--- xitorch/_docstr/api_docstr.py
+++ xitorch/_docstr/api_docstr.py
@@ -1,4 +1,6 @@
-def get_methods_docstr(cls_or_func, methods):
+import inspect
+
+def get_methods_docstr(cls_or_func, methods, ignore_kwargs=None):
     """
     Get the full docstring of a class or a function. Full docstring is the
     main docstring of the class or function plus docstrings of the methods
@@ -13,6 +15,8 @@ def get_methods_docstr(cls_or_func, methods):
         If a list, it lists the methods with the same method name options.
         If a dict, it contains the method name as the keys and the method
         function or class as the value.
+    * ignore_kwargs: list or None
+        List of string on which kwargs to be ignored in addition to ["params"]
 
     Returns
     -------
@@ -22,9 +26,14 @@ def get_methods_docstr(cls_or_func, methods):
     method_template = """
     Methods
     -------
-    method="{name}"
+    ({kwargs_sig})
     """
     res = cls_or_func.__doc__
+
+    def_ignore_kwargs = ["params"]
+    if ignore_kwargs is None:
+        ignore_kwargs = []
+    ignore_kwargs = ignore_kwargs + def_ignore_kwargs
 
     if isinstance(methods, dict):
         generator = methods.items()
@@ -34,12 +43,32 @@ def get_methods_docstr(cls_or_func, methods):
         raise TypeError("methods must be a list or a dict")
 
     for name, method in generator:
+        # get the signatures
+        sigparams = inspect.signature(method).parameters
+        kwargs_sig_list = ['method="%s"'%name] + \
+            ["%s=%s"%(name,val) for name,val in _get_default_parameters(sigparams, ignore_kwargs)]
+        kwargs_sig = ", ".join(kwargs_sig_list)
+
+        # add the method name
         methoddoc = method.__doc__
-        res = res + method_template.format(name=name)
+        res = res + method_template.format(
+            kwargs_sig=kwargs_sig,
+        )
         if methoddoc is not None:
             method_doc = _add_indent(method.__doc__, " "*4)
             res = res + method_doc
     return res
+
+def _get_default_parameters(parameters, ignore_kwargs):
+    empty = inspect.Parameter.empty
+    for paramname in parameters:
+        if paramname in ignore_kwargs:
+            continue
+        defval = parameters[paramname].default
+        if defval == empty:
+            continue
+        defval = defval if not isinstance(defval, str) else '"%s"'%defval
+        yield paramname, defval
 
 def _add_indent(s, indent):
     return "\n".join([indent+line for line in s.split("\n")])
