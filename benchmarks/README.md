bef681d9be19d2e4bfb09527cbbdb4e444fedd01
mfkasim91 <firman.kasim@gmail.com>
2020-10-20 17:54:42 +0100

    Benchmark solve with asv

---
+++ benchmarks/README.md
@@ -0,0 +1,7 @@
+# xitorch
+
+Functionals used in the benchmarks should have the method unspecified, unless
+you are making a benchmark for a specific method / implementation.
+One of the objectives of the benchmarks is to select the best method for the
+functionals that works on wide range of cases in a reasonable amount of time
+(i.e. robustness over speed).
---
+++ benchmarks/benchmarks_solve.py
@@ -0,0 +1,45 @@
+import os
+import time
+import warnings
+import torch
+from xitorch.linalg import solve
+from xitorch import LinearOperator
+from xitorch._utils.tensor import create_random_square_matrix
+
+thisfile_dir = os.path.dirname(os.path.realpath(__file__))
+
+class SolveMatrixTimeSuite:
+    params = [
+        [True, False],
+        [(-1.0, 1.0), (0.0, 1.0), (0.2, 1.0), (0.5, 1.0)],
+        [100, 350, 700],
+    ]
+    param_names = ["is_hermitian", "minmaxeival", "n"]
+
+    def setup(self, is_hermitian, minmaxeival, n):
+        seed = 123
+        ncols = 50
+        torch.manual_seed(seed)
+        min_eival, max_eival = minmaxeival
+        A = create_random_square_matrix(
+            n, is_hermitian=is_hermitian,
+            min_eival=min_eival, max_eival=max_eival,
+            seed=seed)
+        self.A = LinearOperator.m(A)
+        X = torch.randn(n, ncols, dtype=A.dtype)
+        self.B = self.A.mm(X)
+
+    def time_matrix_AB(self, *args, **kwargs):
+        with warnings.catch_warnings(record=True) as ws:
+            X = solve(self.A, self.B)
+            # see warnings about convergence and turns it to an error
+            _catch_convergence_warnings(ws)
+
+def _load_matrix(fname):
+    return torch.load(os.path.join(thisfile_dir, fname))
+
+def _catch_convergence_warnings(ws):
+    for wnings in ws:
+        w = str(wnings.message)
+        if "converge" in w.lower():
+            raise RuntimeError(w)
