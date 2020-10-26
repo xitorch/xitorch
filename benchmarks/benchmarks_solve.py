4e292f1118d41fdf28a63c137c1619f942304dc4
mfkasim91 <firman.kasim@gmail.com>
2020-10-26 15:10:39 +0000

    Benchmarks on symeig

--- benchmarks/benchmarks_solve.py
+++ benchmarks/benchmarks_solve.py
@@ -2,7 +2,7 @@ import os
 import time
 import warnings
 import torch
-from xitorch.linalg import solve
+from xitorch.linalg import solve, symeig
 from xitorch import LinearOperator
 from xitorch._utils.tensor import create_random_square_matrix
 
@@ -25,7 +25,7 @@ class SolveMatrixTimeSuite:
             n, is_hermitian=is_hermitian,
             min_eival=min_eival, max_eival=max_eival,
             seed=seed)
-        self.A = LinearOperator.m(A)
+        self.A = LinearOperator.m(A, is_hermitian=is_hermitian)
         X = torch.randn(n, ncols, dtype=A.dtype)
         self.B = self.A.mm(X)
 
@@ -35,8 +35,29 @@ class SolveMatrixTimeSuite:
             # see warnings about convergence and turns it to an error
             _catch_convergence_warnings(ws)
 
-def _load_matrix(fname):
-    return torch.load(os.path.join(thisfile_dir, fname))
+class SymeigMatrixTimeSuite:
+    params = [
+        [(-1.0, 1.0), (0.0, 1.0), (0.2, 1.0), (0.5, 1.0)],
+        [100, 350, 700],
+    ]
+    param_names = ["minmaxeival", "n"]
+
+    def setup(self, minmaxeival, n):
+        seed = 123
+        ncols = 50
+        torch.manual_seed(seed)
+        min_eival, max_eival = minmaxeival
+        A = create_random_square_matrix(
+            n, is_hermitian=True,
+            min_eival=min_eival, max_eival=max_eival,
+            seed=seed)
+        self.A = LinearOperator.m(A, is_hermitian=True)
+
+    def time_matrix_AB(self, *args, **kwargs):
+        with warnings.catch_warnings(record=True) as ws:
+            X = symeig(self.A, neig=10, mode="lowest")
+            # see warnings about convergence and turns it to an error
+            _catch_convergence_warnings(ws)
 
 def _catch_convergence_warnings(ws):
     for wnings in ws:
