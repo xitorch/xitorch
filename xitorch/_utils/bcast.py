f12cfdd340725b3d0e57be2b7180881ae8e434c1
mfkasim1 <firman.kasim@gmail.com>
2022-01-25 15:04:00 +0000

    Interp1D to accept batched x and xq

--- xitorch/_utils/bcast.py
+++ xitorch/_utils/bcast.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+import torch
+
 def normalize_bcast_dims(*shapes):
     """
     Normalize the lengths of the input shapes to have the same length.
@@ -13,3 +16,12 @@ def get_bcasted_dims(*shapes):
     """
     shapes = normalize_bcast_dims(*shapes)
     return [max(*a) for a in zip(*shapes)]
+
+def match_dim(*xs: torch.Tensor, contiguous: bool = False) -> Tuple[torch.Tensor, ...]:
+    # match the N-1 dimensions of x and xq for searchsorted and gather with dim=-1
+    orig_shapes = tuple(x.shape[:-1] for x in xs)
+    shape = tuple(get_bcasted_dims(*orig_shapes))
+    xs_new = tuple(x.expand(shape + (x.shape[-1],)) for x in xs)
+    if contiguous:
+        xs_new = tuple(x.contiguous() for x in xs_new)
+    return xs_new
